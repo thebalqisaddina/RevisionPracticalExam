37edacde90a2a7fed7037ef6a55bdad12a4138b8
Balqis Addina <thebalqisaddina@gmail.com>
2025-12-15 23:38:58 +0800

    Add Courses model with mcr and routes

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\CourseController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::resource('students', StudentController::class);
+Route::resource('courses', StudentController::class);
+
